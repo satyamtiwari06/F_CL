c2a7106e0b2a63e7600b2f9d4908b20a425a2c57
Satyam Tiwari <st9807419005@gmail.com>
2024-09-10 02:41:43 +0530

    New branch

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api':'http://localhost:3000'
+      '/api':'https://server-chat-8skv.onrender.com'
     }
   },
   plugins: [react()],
